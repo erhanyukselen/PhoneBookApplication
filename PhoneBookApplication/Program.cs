73368593f4238722bfe1eae022f1b1e77750b033
Erhan Ali Yükselen <102170410+erhanyukselen@users.noreply.github.com>
2022-05-21 20:51:53 +0300

    Program.cs OK

--- PhoneBookApplication/Program.cs
+++ PhoneBookApplication/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PhoneBookApplication.Controllers;
+using PhoneBookApplication.Models;
+using System;
+
+namespace PhoneBookApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PhoneBookListModel.phoneBookNumberList.Add(new PhoneBookNumberModel("Erhan", "Yükselen", "123"));
+            PhoneBookListModel.phoneBookNumberList.Add(new PhoneBookNumberModel("Ayberk", "Kalyoncu", "456"));
+            PhoneBookListModel.phoneBookNumberList.Add(new PhoneBookNumberModel("Faruk", "Erol", "789"));
+            PhoneBookListModel.phoneBookNumberList.Add(new PhoneBookNumberModel("Murat", "Aktaş", "321"));
+            PhoneBookListModel.phoneBookNumberList.Add(new PhoneBookNumberModel("Doğukan", "Sevinç", "654"));
+
+
+            OperationController.StartMethots();
+            Console.WriteLine("Çıkmak İçin Herhangi Bir Tuşa Basınız...");
+            Console.ReadKey();
+
+        }
+    }
+}
